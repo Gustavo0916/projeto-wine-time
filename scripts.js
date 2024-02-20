62aef8b1d8a4535f9af627f7810cc15b88962889
Gustavo0916 <117117110+Gustavo0916@users.noreply.github.com>
2024-02-20 12:19:04 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((elements ) => myObserver.observe(elements))
+
+/***************************************************/
+
+/* MENU MOBILE */
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
